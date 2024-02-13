f112d5a416a28fd5ca49d1bb125838eb5bc4ea45
Jasmi Vekariya <jasmivekariya1@gmail.com>
2024-02-13 11:14:42 -0500

    Adding Logic for printing the sum of 1 to 500 using while loop

--- f2.cpp
+++ f2.cpp
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+int main()
+{
+    int x = 1, sum = 0;
+
+    //In the while loop, the condition of x less than or equal to 500 will be evaluated repeatedly and the body part of the loop will be executed till the value of x gets exceed to 500.
+
+    while (x <= 500)
+    {
+
+        sum = sum + x;
+        x++;
+    }
+
+    // Here comes the final answer that is the sum of numbers ranging from 1 to 500.
+
+    printf("%d", sum);
+    return 0;
+}
